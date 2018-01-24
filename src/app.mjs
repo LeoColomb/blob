e9cbeb108bbb9a62ca9c973636878fcd1a96df97
Léo Colombaro <git@colombaro.fr>
2018-01-24 23:28:01 +0100

    first commit

---
+++ src/app.mjs
@@ -0,0 +1,53 @@
+import { Blob } from './blob'
+
+class App {
+  constructor () {
+    cons canvas = document.getElementById('canvas')
+    this.ctx = canvas.getContext('2d')
+
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    this.blobs = []
+    this.blobs.push(new Blob(this.ctx, {
+      fillColor: 'red',
+      shadowColor: 'red'
+    }))
+    this.blobs.push(new Blob(this.ctx, {}))
+    this.frameRequestId = this.frame()
+  }
+
+  destroyer () {
+    window.cancelAnimationFrame(this.frameRequestId)
+  },
+
+  frame () {
+    this.ctx.clearRect(
+      0,
+      0,
+      window.innerWidth,
+      window.innerHeight
+    )
+    for (const blob of this.blobs) {
+      blob.render()
+    }
+    return window.requestAnimationFrame(this.frame)
+  }
+
+  getBlob (blobId) {
+    return this.blobs[blobId].localOptions
+  }
+
+  addBlob (blobOptions = {}) {
+    this.blobs.push(new Blob(this.ctx, blobOptions))
+  }
+
+  deleteBlob (blobId) {
+    this.blobs.pop(blobId)
+  }
+
+  updateBlob (blobId, blobOptions = {}) {
+    this.blobs[blobId].options(blobOptions)
+  }
+}
+
+new App()
---
+++ src/blob.mjs
@@ -0,0 +1,93 @@
+/**
+ * Blob for Graphism.fr
+ * Copyright (c) 2016 Geoffrey Dorne SAS. All Rights Reserved.
+ *
+ * @author 2013 Dionysis Zindros (www.dionyziz.com)
+ * @author 2016-2018 Léo Colombaro (colombaro.fr)
+ */
+export class Blob {
+  /**
+   * @param {CanvasRenderingContext2D} context
+   * @param options
+   */
+  constructor (context, options = {}) {
+    this.time = new Date() | 0
+    this.options(options)
+
+    this.context = context
+    this.radius = window.innerWidth / 800
+    this.generateWaves()
+  }
+
+  options (options = {}) {
+    this.localOptions = Object.assign({
+      width: 80,
+      height: 80,
+      fillColor: '#ffffff',
+      shadowColor: '#ffffff',
+      shadowBlur: 30,
+      waves: 7,
+      thetaResolution: 0.05
+    }, options)
+  }
+
+  generateWaves () {
+    this.waves = []
+    for (let i = 0; i < this.localOptions.waves; ++i) {
+      this.waves[i] = new Wave()
+    }
+  }
+
+  polarFunction (theta) {
+    let r = this.radius
+    for (const wave of this.waves) {
+      r += wave.alpha * Math.sin(wave.omega * theta + wave.phi)
+    }
+    return r * 2
+  }
+
+  drawFrame () {
+    this.context.shadowColor = this.localOptions.shadowColor
+    this.context.shadowBlur = this.localOptions.shadowBlur * this.radius
+    this.context.fillStyle = this.localOptions.fillColor
+    this.context.beginPath()
+    for (let theta = 0; theta < 2 * Math.PI; theta += this.localOptions.thetaResolution) {
+      const r = 65 * this.polarFunction(theta)
+      this.context.lineTo(
+        window.innerWidth / 2 + r * Math.cos(theta),
+        window.innerHeight / 2 + r * Math.sin(theta)
+      )
+    }
+    this.context.fill()
+  }
+
+  integrate (dt) {
+    for (const wave of this.waves) {
+      wave.phi += dt * wave.deltaPhi
+      wave.alpha = wave.maxAlpha * Math.sin(Math.PI * wave.life / wave.lifeTime)
+
+      wave.life += dt
+      if (wave.life > wave.lifeTime) {
+        wave.constructor()
+      }
+    }
+  }
+
+  render () {
+    this.integrate((new Date() | 0) - this.time)
+    this.time = new Date() | 0
+    this.drawFrame()
+  }
+}
+
+class Wave {
+  constructor () {
+    this.maxAlpha = Math.random() / 20
+    this.alpha = 0
+    this.omega = Math.floor(Math.random() * 10)
+    this.phi = Math.random() * 2 * Math.PI
+    this.deltaPhi = (0.5 - Math.random()) / 50
+    this.life = 0
+    this.lifeTime = 1000 + Math.random() * 1000
+  }
+}
